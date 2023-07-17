9cae2b8e0299868a837aa8872a3d5f12cb13651a
adhikari-amit <adhikariamit2001@gmail.com>
2023-07-17 13:12:19 +0530

    Contact Form

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ var typed = new Typed(".multiple-text",{
     backDelay: 1500,
     loop: true
 })
+
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    emailjs.sendForm('service_doj4kli', 'template_hrn0jts', '#contact-form')
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById("success").innerHTML = "I get your message. Will contact you as soon as possible."
+            document.getElementById('contact-form').reset();
+        }, function (error) {
+           document.getElementById("alert").innerHTML = "Something Went Wrong !!!. Please Contact via Mobile or other social Handels."
+            console.log('FAILED...', error);
+        });
+
+});
